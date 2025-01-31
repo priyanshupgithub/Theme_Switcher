210fecba1e70d8848e69eb34484c281923ee63ad
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-01-31 18:00:22 +0530

    Initialise the project with react,vite,tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="font-bold text-3xl text-start underline text-red-400">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
